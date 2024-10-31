c3c6c998e2a07cc9e9d3c275ea29f983397f8cc6
elkomysara <sa1244@fayoum.edu.eg>
2024-10-31 23:14:31 +0300

    comment

---
+++ navgation.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pages
+
+p= [
+st.Page("home.py", title="Home"),
+st.Page("app\streamlit_dashboard.py", title="Dashboards"),
+st.Page("sentiment.py", title="Sentiment", icon="🔥")]
+#pa = pages.nav_pages()
+ng = st.navigation(pages= p, position='sidebar', expanded=False)
+#ng= st.navigation([ st.Page("home.py"), st.Page("dashboard.py") , st.Page("sentiment.py")])
+ng.run()
