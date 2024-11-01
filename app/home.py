5eb51d910f182bbea66a75e005eb1abcc6c698f9
ShHassanin <74568413+ShHassanin@users.noreply.github.com>
2024-11-01 02:26:52 +0200

    Add files via upload

---
+++ app/home.py
@@ -0,0 +1,5 @@
+import streamlit
+
+
+st.set_page_config(page_icon = '1fac0.png',page_title= 'Sentiment Analysis',layout='wide')
+st.title(':chart_with_upwards_trend: Sentiment Analysis Dashboard')
--- app/navgation.py
+++ app/navgation.py
@@ -1,10 +1,8 @@
 import streamlit as st
 
 p= [
-#st.Page("home.py", title="Home"),
+st.Page("home.py", title="Home"),
 st.Page("streamlit_dashboard.py", title="Dashboards"),
 st.Page("sentiment.py", title="Sentiment", icon="🔥")]
-#pa = pages.nav_pages()
 ng = st.navigation(pages= p, position='sidebar', expanded=False)
-#ng= st.navigation([ st.Page("home.py"), st.Page("dashboard.py") , st.Page("sentiment.py")])
 ng.run()
